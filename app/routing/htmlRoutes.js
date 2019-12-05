e9dd9f4040a9569b4ae3cf7ff68fb38fbf7fd281
Francisco Silva <ischawk@gmail.com>
2019-12-05 10:25:09 -0600

    Updating routes for heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,17 +2,17 @@ var path = require("path");
 
 function routes(app){
     app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/home.html"));
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
     app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     app.get("/favicon.ico", function(req, res) {
         console.log("Icon");
         res.end();
 	});
 	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/home.html"));
+		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 }
 
